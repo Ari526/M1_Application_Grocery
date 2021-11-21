d23c76eb575d6bb66a1a6f1a486693ba4a533afc
Ari526 <53427086+Ari526@users.noreply.github.com>
2021-11-21 18:36:13 +0530

    Create Grocery.h

---
+++ 3_Implementation/inc/Grocery.h
@@ -0,0 +1,21 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+void cur(int);
+void departmentbill();
+void department_menu();
+void view_concern();
+void department_search();
+void cal_bill() ;
+void modify();
+void add_stocks();
+
+void delete_stocks();
+void exit();
+
+
+void depart_code();
+void depart_rate();
+void depart_qty();
+void depart_all();
+#endif 
+
